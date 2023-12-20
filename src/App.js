537181f18371f29ec95dfe499e78e29a8a4667fb
OlgaMakeikina <makeikina2202@gmail.com>
2023-12-20 20:15:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+
 import './App.css';
+import Modal from './Modal/Modal';
+import Content from './Modal/Content';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick = { () => setIsOpen(true)}>Open Modal</button>
+      {
+        isOpen && 
+        <Modal setIsOpen={setIsOpen}>
+          <Content setIsOpen={setIsOpen}/>npm start
+        </Modal>
+      }
     </div>
   );
 }
